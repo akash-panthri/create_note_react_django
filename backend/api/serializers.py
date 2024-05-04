ac818a84060d4e4535e4781e2e94d32cc8ddc776
akash-panthri <akashpanthri2000@gmail.com>
2024-05-05 04:13:38 +0530

    Added create function in UserSerializer in serializers.py in api folder in backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,3 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
