08bb9228259e25417db82d076a6fffd5675b8531
akash-panthri <akashpanthri2000@gmail.com>
2024-05-06 04:03:21 +0530

    Added CreateUserView class in views.py in api folder in backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,3 +5,7 @@ from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
