4b88c4a63d9a164814877511ae462ed0858d9bd7
akash-panthri <akashpanthri2000@gmail.com>
2024-05-08 04:01:10 +0530

    Added perform_create in NoteListCreate in views.py in api folder in backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -19,3 +19,8 @@ class NoteListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
