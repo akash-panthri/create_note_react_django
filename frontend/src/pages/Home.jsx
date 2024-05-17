33c4293b68fb5fb52c3ab841d562f17fcea9bb95
akash-panthri <akashpanthri2000@gmail.com>
2024-05-18 04:38:06 +0530

    Modified getNotes function to Home.jsx in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,18 @@ export default function Home() {
   const [notes, setNotes] = useState([]);
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
+  useEffect(() => {
+    getNotes();
+}, []);
   const getNotes = () => {
-    api.get("/api/notes/")
-  }
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((error) => alert(error));
+  };
   return <div>Home</div>;
 }
